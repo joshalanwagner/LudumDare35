adcade401ac065307fe5fb0fc7c99206504d4af4
Josh Wagner <josh.wagner@gmail.com>
2016-04-16 00:19:30 -0700

    spinners

---
+++ Assets/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Vector3 torque;
+
+	void Awake () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () 
+	{
+		rb.AddTorque(torque);
+	}
+}
