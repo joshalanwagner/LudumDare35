5ba87a351db7c9ed6015df038193596a5c6c5696
Josh Wagner <josh.wagner@gmail.com>
2016-04-17 11:43:27 -0700

    level 14 work

---
+++ Assets/PartyPopper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyPopper : MonoBehaviour {
+
+	private GameManager gm;
+	private Vector3 rootPos;
+	public float moveLength = 0.65f;
+	public float downSpeed = 0.01f;
+
+	void Start () 
+	{
+		rootPos = transform.position;
+		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	
+	}
+	
+	void Update () 
+	{
+		float absOsc = Mathf.Abs(gm.oscValue);
+		// need it to pop up quickly (1 frame) and move down slowly.
+		if (absOsc > 0.9f)
+		{
+			transform.position = Vector3.up * absOsc * moveLength + rootPos;
+		} else
+		{
+			transform.position += Vector3.down * downSpeed;
+		}
+
+			   
+	}
+}
